dee6336b42f39e68e92238d4870003e2f55e26fe
unknown <bkrishna1114>
2022-12-19 12:47:08 +0530

    deleted commnad updated

--- Delete commnad.sql
+++ Delete commnad.sql
@@ -1,1 +1,15 @@
-##Delete Commnand...
+##Delete Commnad...
+SET SQL_SAFE_UPDATES = 0;
+
+use balu;
+show tables;
+
+SELECT * FROM marks;
+
+#DELETING THE RECORDS WHO HAD FAILED IN THE EXAMS...
+
+DELETE FROM  MARKS WHERE MATHS<35;
+
+SELECT * FROM MARKS;
+
+#The id 1 is delted as per the condintion ...that it .....
